c15220af70393b0f8d109dc5d3b7fa2f3ec8b398
jinmo <yjmd2222@gmail.com>
2023-08-10 22:30:02 +0900

    데이터 디렉터리 구조 변경 & json YOLO 형식으로 변환하는 스크립트 추가, 코멘트 추가
    Squashed commit of the following:
    
    commit acd91d6cc468f906d316ecb67d0ed6f5d31a8034
    Author: jinmo <yjmd2222@gmail.com>
    Date:   Thu Aug 10 22:19:53 2023 +0900
    
        스크립트 실행시 변환/변경 진행상황 print
    
    commit 4dbedcea4502d8b0979b33cc116f5b7c7d05dce4
    Author: jinmo <yjmd2222@gmail.com>
    Date:   Thu Aug 10 22:13:45 2023 +0900
    
        데이터 디렉터리 구조 변경 & json YOLO 형식으로 변환하는 스크립트 추가

---
+++ restructure_aihub_data.py
@@ -0,0 +1,25 @@
+import os
+
+def restructure_aihub_data(data_root):
+    '''
+    AI-hub 데이터 디렉터리 구조 변경\n
+    원천데이터 -> images폴더 안 name.jpg
+    라벨링데이터 -> labels폴더 안 name.json
+    '''
+    # images, labels 폴더 없으면 생성
+    parent_paths = [os.path.join(data_root, parent) for parent in ['images', 'labels']]
+    for parent_path in parent_paths:
+        os.makedirs(parent_path, exist_ok=True)
+
+    # 디렉터리 구조 재구성
+    for count, (root, _, files) in enumerate(os.walk(data_root)):
+        for file in files:
+            new_parent_path = 'labels' if '.json' in file else 'images'
+            os.popen(f'cp {os.path.join(root, file)} {os.path.join(data_root, new_parent_path, file)}')
+            if count % 100 == 0:
+                print(f'{count}번째 파일: {file}')
+
+    print('디렉터리 구조 변경 완료')
+
+if __name__ == '__main__':
+    restructure_aihub_data('데이터')
