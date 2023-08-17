f17969c0c6b229a7dc0d625367f309a36d360419
jinmo <yjmd2222@gmail.com>
2023-08-17 12:36:47 +0900

    resize 스크립트 가로세로 비율 유지 가능

--- preprocess/resize/resize_640.py
+++ preprocess/resize/resize_640.py
@@ -25,10 +25,16 @@ for folder, subfolders in directory_name.items():
 
 
 # 원본 이미지 resize(1920x1080 → 640x640)하여 저장
-def resize(folder_path, file):
+def resize(folder_path, file, keep_aspect=True):
     '파일 열고 이미지 사이즈 조정 후 저장'
     image = cv2.imread(os.path.join(folder_path,file))
-    image = cv2.resize(image, (640,640))
+    if keep_aspect:
+        h, w = image.shape[:2] # h,w
+        nw = 640
+        nh = int(nw / w * h)
+        image = cv2.resize(image, (nw,nh)) # w,h
+    else:
+        image = cv2.resize(image, (640,640))
     
     # 주의. Team Project2(프로젝트 홈 디렉토리)에 data_pre, images, train/test/val 폴더 존재하지 않으면 이미지 저장안됨.
     save_path = os.path.join(current_path, 'data', 'images_pre', folder, file)
@@ -37,8 +43,8 @@ def resize(folder_path, file):
 
 for folder in ['train','test','val']:
     # # 주의. Team Project2(프로젝트 홈 디렉토리)에 data 폴더, images 폴더, train, test,val 폴더 및 내부 이미지 데이터 존재해야함.
-    folder_path = os.path.join(current_path,'data','images',folder) # ./Team Project2/data/images/ 폴더에 존재하는 train/test/val 각각의 path 접근
-    file_list = os.listdir(os.path.join(current_path,'data','images',folder)) # ↑ 각 폴더 내 이미지 파일 리스트화
+    folder_path = os.path.join(current_path,'data','images_raw',folder) # ./Team Project2/data/images/ 폴더에 존재하는 train/test/val 각각의 path 접근
+    file_list = os.listdir(os.path.join(current_path,'data','images_raw',folder)) # ↑ 각 폴더 내 이미지 파일 리스트화
     # tqdm 라이브러리 사용하여 진행상황 확인.
     progress_bar = tqdm(total=len(file_list), desc= f'{folder} : Data preprocessing is in progress')
     # 병렬
