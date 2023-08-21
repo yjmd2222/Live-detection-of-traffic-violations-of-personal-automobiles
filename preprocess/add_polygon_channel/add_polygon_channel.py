bb233045b730019ee4a50507ce3ecd096eee25aa
jinmo <yjmd2222@gmail.com>
2023-08-21 22:58:08 +0900

    sampling, 4채널 병합 또는 1채널 segmentation 이미지 추출 코드 업로드

---
+++ preprocess/add_polygon_channel/add_polygon_channel.py
@@ -0,0 +1,65 @@
+'레이블 polygon 데이터로 4번째 채널 만들어서 이미지 병합'
+import cv2
+import os
+import numpy as np
+import sys
+from tqdm import tqdm
+from concurrent.futures import ThreadPoolExecutor
+sys.path.append('preprocess/restructure')
+from _settings import seg_yolo_to_aihub_mapper
+
+def add_polygon_channel(old_image_root, new_image_root, progress_bar, merge, file):
+    '이미지에 polygon 추가'
+    file: str
+    # 파일 확인: dir
+    if os.path.isdir(file):
+        progress_bar.update(1)
+        return
+    # 파일 확인: 기타 파일
+    if not (file.endswith('jpg') or file.endswith('jpeg')):
+        progress_bar.update(1)
+        return
+    # 파일 확인: jpg
+    else:
+        # path 생성
+        old_image_file_path = os.path.join(old_image_root, file)
+        new_image_file_path = os.path.join(new_image_root, file).replace('jpg', 'png').replace('jpeg', 'png')
+        txt_file_path = old_image_file_path.replace('jpg', 'txt').replace('jpeg', 'txt').replace('images_raw', 'labels_seg_all')
+    # 원본이미지로부터 dim 추출
+    raw_image = cv2.imread(old_image_file_path)
+    height, width = raw_image.shape[:2]
+    # fourth_channel 빈 이미지 생성
+    fc_image = np.zeros((height, width, 1), np.uint8)
+    # .txt 파일에서 polygon 읽어서 표기
+    with open(txt_file_path, 'r') as txtfile:
+        lines = txtfile.readlines()
+        for line in lines:
+            parts = line.strip().split()
+            class_id = seg_yolo_to_aihub_mapper[int(parts[0])]
+            class_id: int
+            numbers = [float(p) for p in parts[1:]]
+            yolo_points = [numbers[i:i+2] for i in range(0,len(numbers),2)]
+            pixel_points = np.array([(int(point[0] * width), int(point[1] * height)) for point in yolo_points], dtype=np.int64)
+            cv2.fillPoly(fc_image, [pixel_points], class_id)
+    # 파일 저장
+    new_parent_path = os.path.dirname(new_image_file_path)
+    os.makedirs(new_parent_path, exist_ok=True)
+    if merge:
+        merged_image = cv2.merge((raw_image, fc_image))
+        cv2.imwrite(new_image_file_path, merged_image)
+    else:
+        cv2.imwrite(new_image_file_path, fc_image)
+    progress_bar.update(1)
+    return
+
+def main(old_path, new_path, merge):
+    '모든 이미지에 polygon 추가 후 저장'
+    for old_image_root, _, files in os.walk(old_path):
+        new_image_root = old_image_root.replace(old_path, new_path)
+        progress_bar = tqdm(total=len(files), desc=old_image_root)
+        with ThreadPoolExecutor() as executor:
+            executor.map(lambda file: add_polygon_channel(old_image_root, new_image_root, progress_bar, merge, file), files)
+        progress_bar.close()
+
+if __name__ == '__main__':
+    main('data/images_raw', 'D:/data/images_seg_four', True)
---
+++ preprocess/sampling/sampling.py
@@ -0,0 +1,49 @@
+'샘플링 코드'
+import os
+import random
+import shutil
+from tqdm import tqdm
+from concurrent.futures import ThreadPoolExecutor
+
+def sample_files():
+    'data/images/train 등에 있는 파일 1/5 비율로 샘플링'
+    folder_names = ['train', 'val', 'test']
+    image_folder_paths = [os.path.join('data/images/', folder) for folder in folder_names]
+
+    for folder_path in image_folder_paths:
+        for old_image_root, _, image_file_names in os.walk(folder_path):
+            image_file_names = random.sample(image_file_names, len(image_file_names)//5)
+            progress_bar = tqdm(total=len(image_file_names), desc=f'{folder_path} 샘플링')
+            old_image_root: str
+            old_json_root = old_image_root.replace('images', 'labels_json')
+            old_det_txt_root = old_json_root.replace('_json', '_det')
+            old_seg_txt_root = old_det_txt_root.replace('_det', '_seg')
+            new_image_root = old_image_root.replace('images', 'images_samples')
+            new_json_root = new_image_root.replace('images', 'labels_json')
+            new_det_txt_root = new_json_root.replace('_json', '_det')
+            new_seg_txt_root = new_det_txt_root.replace('_det', '_seg')
+            for new_root in [new_image_root, new_json_root, new_det_txt_root, new_seg_txt_root]:
+                os.makedirs(new_root, exist_ok=True)
+            def parallel(image_file_name: str):
+                image_file_name: str
+                json_file_name = image_file_name.split('.')[0] + '.json'
+                txt_file_name = json_file_name.replace('.json', '.txt')
+
+                old_image_file_path = os.path.join(old_image_root, image_file_name)
+                old_json_file_path = os.path.join(old_json_root, json_file_name)
+                old_det_txt_file_path = os.path.join(old_det_txt_root, txt_file_name)
+                old_seg_txt_file_path = os.path.join(old_seg_txt_root, txt_file_name)
+                new_image_file_path = os.path.join(new_image_root, image_file_name)
+                new_json_file_path = os.path.join(new_json_root, json_file_name)
+                new_det_txt_file_path = os.path.join(new_det_txt_root, txt_file_name)
+                new_seg_txt_file_path = os.path.join(new_seg_txt_root, txt_file_name)
+                for path_tuple in zip([old_image_file_path, old_json_file_path, old_det_txt_file_path, old_seg_txt_file_path],
+                                    [new_image_file_path, new_json_file_path, new_det_txt_file_path, new_seg_txt_file_path]):
+                    shutil.copy(*path_tuple)
+                progress_bar.update(1)
+            with ThreadPoolExecutor() as executor:
+                executor.map(parallel, image_file_names)
+            progress_bar.close()
+
+if __name__ == '__main__':
+    sample_files()
