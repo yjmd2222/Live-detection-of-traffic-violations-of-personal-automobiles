486b3f1159f0ab4a774ac7ed87f2699cd457cf05
yjmd2222 <122770065+yjmd2222@users.noreply.github.com>
2023-08-29 15:30:43 +0900

    detect endpoint get에서 video src 추출해서 video 태그에 입력

--- get_video_src.py
+++ get_video_src.py
@@ -8,7 +8,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def get_video_url(iframe_url=r'http://www.utic.go.kr/view/map/cctvStream.jsp?cctvid=L902704&cctvname=%25EC%2584%259C%25EC%259A%25B8%2520%25EC%2584%259C%25EC%25B4%2588%2520%25EC%2584%259C%25EC%25B4%2588%25EB%258C%2580%25EB%25A1%259C&kind=EE&cctvip=9990&cctvch=null&id=null&cctvpasswd=null&cctvport=null&minX=127.00182995981353&minY=37.48782667683961&maxX=127.06121163497251&maxY=37.51313060242616'):
+def get_video_src(iframe_url=r'http://www.utic.go.kr/view/map/cctvStream.jsp?cctvid=L902704&cctvname=%25EC%2584%259C%25EC%259A%25B8%2520%25EC%2584%259C%25EC%25B4%2588%2520%25EC%2584%259C%25EC%25B4%2588%25EB%258C%2580%25EB%25A1%259C&kind=EE&cctvip=9990&cctvch=null&id=null&cctvpasswd=null&cctvport=null&minX=127.00182995981353&minY=37.48782667683961&maxX=127.06121163497251&maxY=37.51313060242616'):
     'iframe_url로부터 video 태그의 src url 추출해서 반환'
     options = Options()
     options.add_argument('--headless') # 백그라운드에서 재생
@@ -26,4 +26,4 @@ def get_video_url(iframe_url=r'http://www.utic.go.kr/view/map/cctvStream.jsp?cct
     return src
 
 if __name__ == '__main__':
-    get_video_url()
+    get_video_src()
